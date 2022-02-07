7a6b641b7bd27e5f1ec9c9e264f5dc343591c7f6
Lucas <lucasazzollinivieira@gmail.com>
2022-02-07 11:10:27 -0300

    feat: extract page DONE

--- src/components/ExtractPage/index.js
+++ src/components/ExtractPage/index.js
@@ -1,46 +1,118 @@
-// import axios from 'axios';
-// import { ThreeDots } from 'react-loader-spinner';
-import { useContext } from 'react';
+import axios from 'axios';
+import dayjs from 'dayjs';
+import { ThreeDots } from 'react-loader-spinner';
+import { useContext, useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 
-import { Container, Header, UserName, ExitButton, ExtractContainer, ButtonContainer, Button } from './style';
+import { Container, Header, UserName, ExitButton, ExtractContainer, ButtonContainer, Button, TotalValue, EntriesContainer, Entry } from './style';
 
 import ExitSymbol from '../../assets/img/exit_symbol.svg';
 import AddSymbol from '../../assets/img/add_symbol.svg';
 import RemoveSymbol from '../../assets/img/remove_symbol.svg';
 import UserContext from '../../contexts/UserContext';
-
-const valor = 0;
+let sum = 0;
 
 export default function ExtractPage() {
-    const { user, token, setLoading } = useContext(UserContext);
+    const { user, token, isLoading, setLoading } = useContext(UserContext);
+
+    const [extract, setExtract] = useState([]);
+    const [total, setTotal] = useState();
+    const [type, setType] = useState('');
 
     const navigate = useNavigate();
 
-    setLoading(false);
+    if (!token) {
+        navigate('/');
+    }
 
-    async function handleExtractInfo() {
+    useEffect(() => {
+        setExtract([]);
+        setLoading(true);
+        handleExtractInfo(token);
+        setLoading(false);
+    }, []);
 
+    useEffect(handleTotal, [extract])
+
+    function handleTotal() {
+        const positive = extract.filter((e) => e.type === 'plus');
+        const negative = extract.filter((e) => e.type === 'minus');
+        let totalPositive = 0;
+        let totalNegative = 0;
+
+        for (let i = 0; i < positive.length; i++) {
+            totalPositive += parseFloat(positive[i].value);
+        }
+        for (let i = 0; i < negative.length; i++) {
+            totalNegative += parseFloat(negative[i].value);
+        }
+
+        sum = (totalPositive - totalNegative).toFixed(2);
+
+        if (sum >= 0) {
+            setType('plus');
+        } else {
+            setType('minus');
+        }
+
+        setTotal(sum);
+
+    };
+
+    async function handleExtractInfo(auth) {
+        setLoading(true)
+        try {
+            const promise = await axios.get('http://localhost:5000/entries', {
+                headers: {
+                    Authorization: `Bearer ${auth}`
+                }
+            });
+
+            setExtract(promise.data.entries);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+            navigate('/');
+        }
     }
 
     return (
         <Container>
             <Header>
-                <UserName>Olá, {user.name}</UserName>
+                <UserName>Olá, {user?.name}</UserName>
                 <ExitButton onClick={() => navigate('/')}><img src={ExitSymbol} alt='exit_button_symbol' /></ExitButton>
             </Header>
             <ExtractContainer>
-                {valor === 0 && <span>Não há registros de <br /> entrada ou saída</span>}
-
+                {isLoading && <span><ThreeDots color='#8C11BE' height={25} width={150} /></span>}
+                {extract.length === 0 && !isLoading && <span>Não há registros de <br /> entrada ou saída</span>}
+                {extract.length !== 0 &&
+                    <>
+                        <EntriesContainer>
+                            {extract.map((element, id) =>
+                                <Entry isPositive={element.type} key={id}>
+                                    <div>
+                                        <span className='date'>{dayjs(element.date).format('DD/MM')}</span>
+                                        <span className='description'>{element.description}</span>
+                                    </div>
+                                    <div className='value'>{(parseFloat(element.value)).toFixed(2).replace('.', ',')}</div>
+                                </Entry>
+                            )}
+                        </EntriesContainer>
+                        <TotalValue isPositive={type}>
+                            <span className='total'>SALDO</span>
+                            <span className='totalValue'>{total}</span>
+                        </TotalValue>
+                    </>
+                }
             </ExtractContainer>
             <ButtonContainer>
                 <Button margin="margin-right: 10px;" onClick={() => navigate('/add')}>
                     <img src={AddSymbol} alt='add_symbol' />
-                    <span>Nova Entrada</span>
+                    <span>Nova entrada</span>
                 </Button>
                 <Button margin="margin-left: 10px;" onClick={() => navigate('/remove')}>
-                    <img src={RemoveSymbol} alt='add_symbol' />
-                    <span>Nova Saída</span>
+                    <img src={RemoveSymbol} alt='remove_symbol' />
+                    <span>Nova saída</span>
                 </Button>
             </ButtonContainer>
         </Container>
