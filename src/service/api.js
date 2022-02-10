b5eb4a25c58e8e4278e187dcafd8cf78b0e10462
Lucas Azzolini Vieira <lucasazzollinivieira@gmail.com>
2022-02-10 16:26:30 -0300

    fix: BASE_URL fixed

--- src/service/api.js
+++ src/service/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const BASE_URL = 'http://localhost:5000';
+const BASE_URL = 'https://my-wallet-datab.herokuapp.com';
 
 function createHeaders(token) {
     return { headers: { Authorization: `Bearer ${token}` } };
